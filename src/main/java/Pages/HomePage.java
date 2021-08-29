5211be074dacc50510812605c227b27fc4cf836c
Anamika <raoanamika36@gmail.com>
2021-08-29 13:25:51 +0530

    all test cases logic

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -1,11 +1,15 @@
 package Pages;
 
 import CommonMethod.Setup;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePage extends Setup {
     public WebDriver driver;
@@ -15,18 +19,17 @@ public class HomePage extends Setup {
         PageFactory.initElements(driver, this);
     }
 
-    @FindBy( css= ".btn.buy")
+    @FindBy(css = ".btn.buy")
     WebElement buynow;
 
     public void clickbunnowbutton() {
         buynow.click();
     }
 
-  @FindBy(xpath = "//div[@class='cart-inner']//span[1]")
+    @FindBy(xpath = "//div[@class='cart-inner']//span[1]")
     WebElement ShoppingCart;
 
-    public WebElement titleShoppingcart()
-    {
+    public WebElement titleShoppingcart() {
         return ShoppingCart;
 
     }
@@ -34,62 +37,144 @@ public class HomePage extends Setup {
     @FindBy(xpath = "//td[@class='amount']")
     WebElement totalamount;
 
-    public WebElement verifytotalamount()
-    {
-        return  totalamount;
+    public WebElement verifytotalamount() {
+        return totalamount;
     }
 
     @FindBy(xpath = "//div[@class='cart-checkout']")
     WebElement checkbtn;
 
-    public WebElement checkoutbutton()
-    {
+    public WebElement checkoutbutton() {
         return checkbtn;
     }
 
-    @FindBy(xpath="//input[@value='Budi']")
+    //Form Data
+    @FindBy(xpath = "//input[@value='Budi']")
     WebElement name;
-    public void namedetails(String name1)
-    {
+
+    public void namedetails(String name1) {
         name.clear();
         name.sendKeys(name1);
 
     }
-    @FindBy(xpath="//input[@value='budi@utomo.com']")
+
+    @FindBy(xpath = "//input[@value='budi@utomo.com']")
     WebElement email;
-    public void emaildetails(String email1)
-    {
+
+    public void emaildetails(String email1) {
         email.clear();
         email.sendKeys(email1);
     }
-    @FindBy(xpath="//input[@value='081808466410']")
+
+    @FindBy(xpath = "//input[@value='081808466410']")
     WebElement phonenumber;
-    public void phonedetails(String phonenumber1)
-    {
+
+    public void phonedetails(String phonenumber1) {
         phonenumber.clear();
         phonenumber.sendKeys(phonenumber1);
     }
-    @FindBy(xpath="//input[@value='Jakarta']")
+
+    @FindBy(xpath = "//input[@value='Jakarta']")
     WebElement city;
-    public void citydetails(String city1)
-    {
+
+    public void citydetails(String city1) {
         city.clear();
         city.sendKeys(city1);
     }
-    @FindBy(xpath="//textarea[normalize-space()='MidPlaza 2, 4th Floor Jl.Jend.Sudirman Kav.10-11']")
+
+    @FindBy(xpath = "//textarea[normalize-space()='MidPlaza 2, 4th Floor Jl.Jend.Sudirman Kav.10-11']")
     WebElement address;
-    public void addressdetails(String address1)
-    {
+
+    public void addressdetails(String address1) {
         address.clear();
         address.sendKeys(address1);
     }
-    @FindBy(xpath="//input[@value='10220']")
-    WebElement postal;
-    public void postalcodedetails(String postal1)
 
-    {
+    @FindBy(xpath = "//input[@value='10220']")
+    WebElement postal;
+
+    public void postalcodedetails(String postal1) {
         postal.clear();
         postal.sendKeys(postal1);
+    }
+
+
+    //Card Data
+
+    @FindBy(xpath = "//input[@name = 'cardnumber']")
+    WebElement debitcard;
+
+    public void credicarddetails(String card) {
+        debitcard.clear();
+        debitcard.sendKeys(card);
+    }
+
+    @FindBy(xpath = "//input[@placeholder='MM / YY']")
+    WebElement expDate;
+
+    public void expDatedetails(String exp) {
+        expDate.clear();
+        expDate.sendKeys(exp);
+    }
+
+    @FindBy(xpath = "//input[@placeholder = '123']")
+    WebElement cvvNumber;
+
+    public void cvvdetails(String cvv) {
+        cvvNumber.clear();
+        cvvNumber.sendKeys(cvv);
+    }
+
+    @FindBy(xpath = "//a[@class='button-main-content']")
+    WebElement submitpayment;
+
+    public WebElement paymentsubmit() {
+        return submitpayment;
+    }
+
+    //transactionData
+
+    @FindBy(xpath = "//p[@id='merchant_name']")
+    WebElement merchnatName;
+
+    public WebElement merchantName() {
+        return merchnatName;
+    }
+
+    @FindBy(xpath = "//p[@id='txn_amount']")
+    WebElement totalPayblAmount;
+
+    public WebElement payblAmount() {
+        return totalPayblAmount;
+    }
+
+    @FindBy(xpath = "//p[@id='card_number']")
+    WebElement Cardnumber;
+
+    public WebElement carnumberdetail() {
+        return Cardnumber;
+    }
+
+    @FindBy(xpath = "//input[@type='password']")
+    WebElement cardpassword;
+
+    public void cardPindetail(String pin) {
+        cardpassword.clear();
+        cardpassword.sendKeys(pin);
+    }
+
+    @FindBy(xpath = "//button[normalize-space()='OK']")
+    WebElement cardOkbutton;
+
+    public WebElement cardOkbuttondetails() {
+        return cardOkbutton;
+    }
+
+    @FindBy(xpath = "//button[normalize-space()='Cancel']")
+    WebElement cardCancelbutton;
+
+    public WebElement cardCancelbutton() {
+        return cardCancelbutton;
     }
 
 }
--- src/main/java/TestCases/Testcases.java
+++ src/main/java/TestCases/Testcases.java
@@ -3,12 +3,14 @@ package TestCases;
 import CommonMethod.Setup;
 import Pages.HomePage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,6 +18,7 @@ import org.testng.annotations.Test;
 import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import javax.swing.plaf.synth.SynthEditorPaneUI;
 import java.io.IOException;
+import java.sql.Driver;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -36,6 +39,8 @@ public class Testcases {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
+
+
 
     //@Test
     public void verifyTitle() {
@@ -66,32 +71,32 @@ public class Testcases {
     }
 
     @Test(priority = 3)
-    public void veriycheckoutbuttondisplay() throws InterruptedException
-    {
+    public void veriycheckoutbuttondisplay() throws InterruptedException {
         Thread.sleep(9000);
         homePage.clickbunnowbutton();
-        WebDriverWait wait = new WebDriverWait(driver,30);
+        WebDriverWait wait = new WebDriverWait(driver, 30);
         wait.until(ExpectedConditions.visibilityOf(homePage.checkoutbutton()));
         //boolean checkoutbtn=homePage.checkoutbutton().isDisplayed();
         Assert.assertTrue(homePage.checkoutbutton().isDisplayed());
 
     }
+
     @Test(priority = 4)
     public void verifylabel() throws InterruptedException {
         Thread.sleep(9000);
         homePage.clickbunnowbutton();
         Thread.sleep(9000);
-        List<WebElement> label =driver.findElements(By.xpath("//td[@class='input-label']"));
+        List<WebElement> label = driver.findElements(By.xpath("//td[@class='input-label']"));
         Thread.sleep(9000);
         List Expctedlist = new ArrayList();
-        for(WebElement l1 :label)
-        {
+        for (WebElement l1 : label) {
             Expctedlist.add(l1.getText());
         }
         System.out.print(Expctedlist);
-        Assert.assertEquals(Expctedlist.toString(),"[Name, Email, Phone no, City, Address, Postal Code]");
+        Assert.assertEquals(Expctedlist.toString(), "[Name, Email, Phone no, City, Address, Postal Code]");
 
     }
+
     @Test
     public void addData() throws InterruptedException {
         Thread.sleep(9000);
@@ -122,12 +127,14 @@ public class Testcases {
         Thread.sleep(9000);
         homePage.checkoutbutton().click();
         driver.switchTo().frame(0);
-        String pagetitle =driver.findElement(By.xpath("//p[@class='text-page-title-content']")).getText();
+        String pagetitle = driver.findElement(By.xpath("//p[@class='text-page-title-content']")).getText();
         System.out.println(pagetitle);
     }
-//Verify name and amount is visible
+
+    //Verify name and amount is visible
     @Test
-    public void verifyData() throws InterruptedException {Thread.sleep(9000);
+    public void verifyData() throws InterruptedException {
+        Thread.sleep(9000);
         homePage.clickbunnowbutton();
         Thread.sleep(9000);
         homePage.checkoutbutton().click();
@@ -136,8 +143,8 @@ public class Testcases {
 //        System.out.println(amount);
 //        String name=driver.findElement(By.xpath("//span[@class='item-name']")).getText();
 //         System.out.println(name);
-       Assert.assertEquals(driver.findElement(By.xpath("//span[@class='text-amount-amount']")).getText(),"20,000");
-        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='item-name']")).getText(),"Midtrans Pillow");
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='text-amount-amount']")).getText(), "20,000");
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='item-name']")).getText(), "Midtrans Pillow");
     }
 
     //verify click on continue button select payment page open
@@ -150,8 +157,9 @@ public class Testcases {
         driver.switchTo().frame(0);
         Thread.sleep(9000);
         driver.findElement(By.xpath("//a[@class='button-main-content']")).click();
-        Assert.assertEquals(driver.findElement(By.xpath("//p[@class='text-page-title-content']")).getText(),"Select Payment");
+        Assert.assertEquals(driver.findElement(By.xpath("//p[@class='text-page-title-content']")).getText(), "Select Payment");
     }
+
     //Verify All payment Methods
     @Test
     public void paymentMethod() throws InterruptedException {
@@ -163,14 +171,13 @@ public class Testcases {
         Thread.sleep(9000);
         driver.findElement(By.xpath("//a[@class='button-main-content']")).click();
         Thread.sleep(9000);
-       List<WebElement> paymentname =driver.findElements(By.xpath("//div[@class='list-title text-actionable-bold']"));
-       ArrayList metholist = new ArrayList();
-       for(WebElement pay:paymentname )
-       {
-           metholist.add(pay.getText());
-          System.out.println(metholist);
-       }
-       //Assert.assertEquals(metholist.toString(),"[Credit/Debit Card, ATM/Bank Transfer, GoPay/other e-Wallets, ShopeePay/other e-Wallets, KlikBCA, BCA KlikPay, OCTO Clicks, Danamon Online Banking, e-Pay BRI, LINE Pay e-cash | mandiri e-cash, Telkomsel Cash, Indomaret, Alfa Group, Akulaku]");
+        List<WebElement> paymentname = driver.findElements(By.xpath("//div[@class='list-title text-actionable-bold']"));
+        ArrayList metholist = new ArrayList();
+        for (WebElement pay : paymentname) {
+            metholist.add(pay.getText());
+        //    System.out.println(metholist);
+        }
+       Assert.assertEquals(metholist.toString(),"[Credit/Debit Card, ATM/Bank Transfer, GoPay/other e-Wallets, ShopeePay/other e-Wallets, KlikBCA, BCA KlikPay, OCTO Clicks, Danamon Online Banking, e-Pay BRI, LINE Pay e-cash | mandiri e-cash, Telkomsel Cash, Indomaret, Alfa Group, Akulaku]");
 
     }
     //verify clicking on debitcard it redirect to card details page
@@ -187,4 +194,185 @@ public class Testcases {
         Thread.sleep(9000);
         driver.findElement(By.xpath("//a[@class='list with-promo']//div[@class='list-content']//div[1]")).click();
     }
+
+    //verify amount changes when user apply promo code
+    @Test
+    public void promoCode() throws InterruptedException {
+        homePage.clickbunnowbutton();
+        Thread.sleep(9000);
+        homePage.checkoutbutton().click();
+        Thread.sleep(9000);
+        driver.switchTo().frame(0);
+        Thread.sleep(9000);
+        driver.findElement(By.xpath("//a[@class='button-main-content']")).click();
+        Thread.sleep(9000);
+        driver.findElement(By.xpath("//a[@class='list with-promo']//div[@class='list-content']//div[1]")).click();
+        Thread.sleep(9000);
+
+        driver.findElement(By.xpath("//div[@class='input-row']//div[2]//div[1]//label[1]")).isEnabled();
+        String pp = driver.findElement(By.xpath("//span[@class='text-amount-amount']")).getText();
+        System.out.println(pp);
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='text-amount-amount']")).getText(), "18,000");
+        System.out.println("I am first checkbox is selcted");
+        Thread.sleep(3000);
+
+        System.out.println("I am checking first the checkbox");
+        driver.findElement(By.xpath("//div[@class='input-row']//div[1]//div[1]//label[1]")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='text-amount-amount']")).getText(), "19,990");
+
+        System.out.println("I am checking third  checkbox");
+        driver.findElement(By.xpath("//div[@class='input-row']//div[3]//div[1]//label[1]")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.xpath("//span[@class='text-amount-amount']")).getText(), "19,000");
+    }
+
+    //verify carddetails and submit
+    @Test
+    public void cardinput() throws InterruptedException {
+
+        homePage.clickbunnowbutton();
+        Thread.sleep(9000);
+        homePage.checkoutbutton().click();
+        Thread.sleep(9000);
+        driver.switchTo().frame(0);
+        Thread.sleep(9000);
+        driver.findElement(By.xpath("//a[@class='button-main-content']")).click();
+        Thread.sleep(9000);
+        driver.findElement(By.xpath("//a[@class='list with-promo']//div[@class='list-content']//div[1]")).click();
+        Thread.sleep(9000);
+        homePage.credicarddetails(Setup.properties.getProperty("card"));
+        homePage.expDatedetails(Setup.properties.getProperty("expDate"));
+        homePage.cvvdetails(Setup.properties.getProperty("cvv"));
+        homePage.paymentsubmit().click();
+    }
+
+    //Verify gateaydetails
+    @Test
+    public void transcationConfirmation() throws InterruptedException {
+        homePage.clickbunnowbutton();
+        Thread.sleep(9000);
+        homePage.checkoutbutton().click();
+        Thread.sleep(9000);
+        driver.switchTo().frame(0);
+        Thread.sleep(9000);
+        driver.findElement(By.xpath("//a[@class='button-main-content']")).click();
+        Thread.sleep(9000);
+        driver.findElement(By.xpath("//a[@class='list with-promo']//div[@class='list-content']//div[1]")).click();
+        Thread.sleep(9000);
+        homePage.credicarddetails(Setup.properties.getProperty("card"));
+        homePage.expDatedetails(Setup.properties.getProperty("expDate"));
+        homePage.cvvdetails(Setup.properties.getProperty("cvv"));
+        homePage.paymentsubmit().click();
+
+        Thread.sleep(9000);
+
+        driver.switchTo().frame(0);
+
+        Assert.assertEquals(homePage.merchantName().getText(), "Rubicon Store");
+        Assert.assertEquals(homePage.payblAmount().getText(), "18000.00");
+        Assert.assertEquals(homePage.carnumberdetail().getText(), "481111-1114");
+    }
+
+
+    //Your card got declined by the bank
+
+    @Test
+    public void verifycorrectpin() throws InterruptedException {
+        homePage.clickbunnowbutton();
+        Thread.sleep(2000);
+        homePage.checkoutbutton().click();
+        Thread.sleep(2000);
+        driver.switchTo().frame(0);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@class='button-main-content']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@class='list with-promo']//div[@class='list-content']//div[1]")).click();
+        Thread.sleep(2000);
+        homePage.credicarddetails(Setup.properties.getProperty("card"));
+        homePage.expDatedetails(Setup.properties.getProperty("expDate"));
+        homePage.cvvdetails(Setup.properties.getProperty("cvv"));
+        homePage.paymentsubmit().click();
+
+        Thread.sleep(9000);
+
+        driver.switchTo().frame(0);
+
+        homePage.cardPindetail(Setup.properties.getProperty("correctpin"));
+        Thread.sleep(2000);
+        homePage.cardOkbuttondetails().click();
+        Thread.sleep(2000);
+
+        driver.switchTo().frame(0);
+   //  String succesmsg = driver.findElement(By.xpath("//div[@class='final-panel success']")).getText();
+   //     String succesmsg = driver.findElement(By.xpath("//div[@text()='Transaction successful]"));
+        //   System.out.println(succesmsg);
+
+
+    }
+
+    //imcorrectpin
+    @Test
+    public void verifyincorrectpin() throws InterruptedException {
+
+        homePage.clickbunnowbutton();
+        Thread.sleep(2000);
+        homePage.checkoutbutton().click();
+        Thread.sleep(2000);
+        driver.switchTo().frame(0);
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@class='button-main-content']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//a[@class='list with-promo']//div[@class='list-content']//div[1]")).click();
+        Thread.sleep(2000);
+        homePage.credicarddetails(Setup.properties.getProperty("card"));
+        homePage.expDatedetails(Setup.properties.getProperty("expDate"));
+        homePage.cvvdetails(Setup.properties.getProperty("cvv"));
+        homePage.paymentsubmit().click();
+
+        Thread.sleep(9000);
+
+        driver.switchTo().frame(0);
+
+        homePage.cardPindetail(Setup.properties.getProperty("incorrectpin"));
+        Thread.sleep(2000);
+        homePage.cardOkbuttondetails().click();
+        Thread.sleep(2000);
+
+        driver.switchTo().frame(0);
+        String failsmsg = driver.findElement(By.xpath("//span[normalize-space()='Your card got declined by the bank']")).getText();
+               System.out.println(failsmsg);
+    }
+
+    @Test
+    public void veriyCancelbutton() throws InterruptedException {
+
+        homePage.clickbunnowbutton();
+        Thread.sleep(8000);
+        homePage.checkoutbutton().click();
+        Thread.sleep(9000);
+        driver.switchTo().frame(0);
+        Thread.sleep(9000);
+        driver.findElement(By.xpath("//a[@class='button-main-content']")).click();
+        Thread.sleep(9000);
+        driver.findElement(By.xpath("//a[@class='list with-promo']//div[@class='list-content']//div[1]")).click();
+        Thread.sleep(9000);
+        homePage.credicarddetails(Setup.properties.getProperty("card"));
+        homePage.expDatedetails(Setup.properties.getProperty("expDate"));
+        homePage.cvvdetails(Setup.properties.getProperty("cvv"));
+        homePage.paymentsubmit().click();
+
+        Thread.sleep(2000);
+
+        driver.switchTo().frame(0);
+        Thread.sleep(2000);
+        homePage.cardCancelbutton().click();
+        Thread.sleep(2000);
+        driver.switchTo().frame(0);
+        String failsmsg = driver.findElement(By.xpath("//span[normalize-space()='Transaction failed']")).getText();
+        System.out.println(failsmsg);
+        Assert.assertEquals(driver.findElement(By.xpath("//span[normalize-space()='Transaction failed']")).getText(),"Transaction failed");
+
+    }
+
 }
